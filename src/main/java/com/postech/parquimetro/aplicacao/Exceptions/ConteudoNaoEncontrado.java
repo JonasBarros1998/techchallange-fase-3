3a9f83cc53cba01163e5e7affe3d11e4d934829d
Jonas Barros <Jonas_barros@outlook.com>
2023-10-03 22:34:40 -0300

    classe para capturar erros relacionados a informaçoes nao encontradas no banco de dados

---
+++ src/main/java/com/postech/parquimetro/aplicacao/Exceptions/ConteudoNaoEncontrado.java
@@ -0,0 +1,8 @@
+package com.postech.parquimetro.aplicacao.Exceptions;
+
+public final class ConteudoNaoEncontrado extends RuntimeException {
+
+	public ConteudoNaoEncontrado(String message) {
+		super(message);
+	}
+}
