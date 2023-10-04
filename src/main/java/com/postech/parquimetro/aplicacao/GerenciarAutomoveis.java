e1e1d10e7761bf7d726a394c0a6778f0f45372ee
Jonas Barros <Jonas_barros@outlook.com>
2023-10-03 22:28:24 -0300

    criando classe e metodo para salvar os dados do veiculo no banco de dados

---
+++ src/main/java/com/postech/parquimetro/aplicacao/GerenciarAutomoveis.java
@@ -0,0 +1,45 @@
+package com.postech.parquimetro.aplicacao;
+
+import com.postech.parquimetro.aplicacao.DTO.AutomovelDTO;
+import com.postech.parquimetro.dominio.entities.Automovel;
+import com.postech.parquimetro.infra.repository.AutomovelRepository;
+import com.postech.parquimetro.infra.repository.CondutorRepository;
+import com.postech.parquimetro.view.form.AutomovelForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GerenciarAutomoveis {
+
+	AutomovelRepository automovelRepository;
+
+	CondutorRepository condutorRepository;
+
+	@Autowired
+	GerenciarAutomoveis(AutomovelRepository automovelRepository, CondutorRepository condutorRepository) {
+		this.automovelRepository = automovelRepository;
+		this.condutorRepository = condutorRepository;
+
+	}
+
+	public AutomovelDTO salvar(AutomovelForm automovelForm) {
+		var automovelDTO = AutomovelDTO.converterAutomovelFormParaAutomovelDTO(automovelForm);
+
+		var condutor = this.condutorRepository.findById(automovelDTO.condutor())
+			.orElseThrow(() -> {
+				throw new RuntimeException("Condutor nao encontrado");
+			});
+
+		var automovel = new Automovel(
+			automovelDTO.placa(),
+			automovelDTO.modelo(),
+			automovelDTO.tipoAutomovel(),
+			condutor
+		);
+
+		this.automovelRepository.save(automovel);
+
+		return automovelDTO;
+	}
+
+}
